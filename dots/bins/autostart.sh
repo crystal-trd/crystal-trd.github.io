790c3ff2fecfd02a534700d5a810d3c5e2271a94
Crystal the trans dev <crystal-td@proton.me>
2022-09-01 09:32:10 +0100

    fixes and tests

--- dots/bins/autostart.sh
+++ dots/bins/autostart.sh
@@ -18,8 +18,6 @@ setxkbmap -layout us -option compose:sclk
 ## Add your autostart programs here --------------
 emacs --daemon & 
 
-docker run -d --rm -p 8080:8080 -it forscht/ddrive --port 8080 --token OTgzMTA5Nzg1NDc2MDA1ODk4.G43xHO.nEQ25Px8jLGiLwFPx7crccmEXeF3FNgulgoKQ8 --channelId 999040576764793044
-docker run -d --rm -p 8888:8080 -v "/home/crystal/my-instance/searxng:/etc/searxng" -e "BASE_URL=http://localhost:8888/" searxng/searxng
 ## -----------------------------------------------
 # Launch DWM
 ~/Crystalwm/bins/dwm-flexi/dwm
